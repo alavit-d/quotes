352608d2cd5010c66a5c6ff4322855ca167d9743
Aymeric Lavit d'Hautefort <alavit-d@student.42.fr>
2015-07-09 14:54:01 +0200

    cleaned code, added famous quote deformation

---
+++ dictionary.js
@@ -0,0 +1,43 @@
+//var verbs = ['creates', 'promotes', 'lives', 'harnesses', 'enjoys', 'feels', 'is', 'gives', 'believes', 'changes', 'dreams', 'lifts', 'makes'];
+var adjs = ['aware', 'healthy', 'strong', 'deep', 'human', 'great', 'small', 'little', 'quiet', 'high', 'heavy', 'deep', 'calm'];
+var conj = [', but', ', then', ', and', ', no', '.', ', because'];
+var famous = ["Adolf Hitler ","George Washington" ,"Emperor Constantine" ,"Martin Luther ","Socrates ","Mahatma Gandhi ","Karl Marx ","Napoleon Bonaparte","Simon Bolivar ","Franklin D. Roosevelt","Charles Darwin ","Sir Isaac Newton","Confucius ","Queen Victoria ","Konrad Adenauer ","Ramses II ","Benjamin Franklin" ,"Alexander the Great","Christopher Columbus" ,"Marcus Aurelius ","Galileo Galilei ","Plato ","Joan of Arc ","Charlemagne ","Aristotle ","Saladin ","Voltaire","Charles Baudelaire","Arthut Rimbaud","Catherine the Great","Mozart","Leonardo da Vinci" ,"Louis Pasteur ","Leo Tolstoy ","Albert Einstein ","Ataturk ","Pablo Picasso ","Benedicto XVI","Margaret Thatcher" ,"Mohammed Ali","John F. Kennedy","Vladimir Putin","Simone de Beauvoir","Marc Zuckerberg","Rosa Parks","Benazir Bhutto ","J.S. Bach ","14th Dalai Lama","Malcolm X ","Lech Walesa ","Charles de Gaulle" ,"Joseph Stalin ","Marie Curie","Giuseppe Garibaldi","Johann Gutenberg" ,"Oliver Cromwell","Vladimir Ilitch Lenin ","Sigmund Freud","Mother Teresa ","Bill Gates","Ernest Hemingway" ,"John Lennon ","Genghis Kahn ","John M Keynes ","Susan B. Anthony" ,"George Orwell ","Thomas Edison ","Dwight Eisenhower","Eleanor Roosevelt ","Dr. B.R. Ambedkar","Nikola Tesla ","Alexander Fleming" ,"Samuel Johnson ","Eva Peron ","Marilyn Monroe", "Henry Ford","Princess Diana ","Oscar Wilde ","Beethoven ","Aung San Suu Kyi", 'Steve Jobs', 'Nicki Minaj', 'Mao Zedong', "Dumbledore", "Gandalf", "Joseph Goebbels", "Erwin Rommel", "Sun Tzu", "Laozi", "Darth Vader", 'Tupac'];
+var pics = ['amazing-736881_640.jpg', 'birds-707348_640.jpg',  'groynes-339252_640.jpg', 'run.jpg', 'sunset-658583_640.jpg', 'bird-383245_640.jpg', 'bricks-459299_640.jpg', 'langhe-390108_1280.jpg', 'sunrise-77677_640.jpg', 'sunset-675847_1280.jpg'];
+var subj3 = ['life', 'success', 'the mind', 'impact', 'wisdom'];
+var subj1 = ['I'];
+var subj2 = ['you', 'we', 'people'];
+var subject = [subj1, subj2, subj3];
+var verbs1 = ['miss the', 'am about making', 'am about', 'am living the', 'am the', 'become the', 'am not the', 'would want'];
+var verbs2 = ['miss the', 'are about making', 'are about', 'are living the', 'are the', 'become the', 'are not the', 'would want'];
+var verbs3 = ['misses the', 'is about making', 'is about', 'is living the', 'is the', 'becomes the', 'is not the', 'would want'];
+var after = ['of man', 'of nature', 'less traveled by', 'like a tree', 'in life', 'within you', 'with humanity'];
+var cplt = ['success', 'boredom', 'passion', 'risks', 'success', 'revenge', 'wisdom', 'consciousness', 'vision', 'dream', 'belief', 'money', 'time', 'challenge', 'time', 'day', 'voice'];
+var mind = ['wisdom', 'consciousness', 'vision', 'dream', 'belief', 'fear'];
+var self = ['power', 'strength', 'success', 'challenge', 'life', 'failure', 'money'];
+var vast = ['time', 'day', 'ocean', 'world', 'shore'];
+var human = ['child', 'elder', 'voice', 'artist', 'day', 'hand', 'father', 'mother'];
+//@@ == Singular, !@@ == inverse of singular, ## == verb, && == plural
+var sing = ['depressed robot', 'message of god to his creation','mass murder', 'human sacrifice', 'satanism', 'blood bath','dictatorship', 'masturbation with an hamster', 'sovietism', 'vibrator', 'illuminati', 'your mom'];
+var oppSing = ['happy dolphin', 'tax inspector','baby-boom', 'new virgin', 'mormonism', 'champagne shower', 'veganism', 'meditation', 'capitalism', 'towel', 'reptil', 'dildo in a box'];
+var verb = ['blaze', 'masturbate', 'sniff', 'screw', 'inject', 'act like Zaphod Beeblebrox', 'hitch-hike', 'fart in your space suit'];
+var phrases =
+['You miss 100% of the @@ you don’t take'
+,'@@ is about making an impact'
+,'@@ is everything'
+,'An unexamined @@ is not worth living'
+,'Eighty percent of @@ is showing up'
+,'The best revenge is massive @@'
+,'Believe you can ## and you’re halfway there'
+,'I would rather die of passion than of @@'
+,'You can ## anything, but not everything'
+,'Courage is not the absence of @@, but rather the judgement that something else is more important than !@@'
+,'@@ is achieved, not when there is nothing more to add, but when there is nothing left to take away'
+,'The richest @@ is not he who has the most, but he who needs the least'
+,'To the man who only has @@, everything he encounters begins to look like a !@@'
+,' We are what we repeatedly do; to ##, then, is not an act but a habit'
+,' A wise man gets more use from his @@ than a fool from his !@@'
+,' Do not seek to follow in the footsteps of the men of old; seek what they ##'
+,' The real voyage of discovery consists not in seeking @@ but seeing with !@@'
+,' Work like you don’t need money, love like you’ve never been hurt, and ## like no one’s watching'
+,' Try to ## three times. Once, to get over the fear of doing it. Twice, to learn how to do it. And a third time, to figure out whether you like it or not'
+,' Even if you’re on the right @@, you’ll get run over if you just sit there'];
---
+++ functions.js
@@ -0,0 +1,48 @@
+function capitalize(str) {
+    //var index = -1;
+    str = str.charAt(0).toUpperCase() + str.slice(1);
+    str = str.replace(/\. [a-z]/g, function myFun(x) { return x.toUpperCase();});
+    return str;
+}
+function getParameterByName(name) {
+    name = name.replace(/[\[]/, "\\[").replace(/[\]]/, "\\]");
+    var regex = new RegExp("[\\?&]" + name + "=([^&#]*)"),
+        results = regex.exec(location.search);
+    return results === null ? "" : decodeURIComponent(results[1].replace(/\+/g, " "));
+}
+function rd(n) {
+    return (n[Math.floor(Math.random() * n.length)]);
+}
+
+//Experimental tryouts for tone of Quote.
+var words = [mind, self, vast, human];
+function genTone() {
+    var tone = Math.floor(Math.random() * words.length);
+    return (rd(words[tone]) + ' ' + rd(verbs) + ' ' + rd(words[tone]));
+}
+function genWord() {
+    return rd(rd(words));
+}
+function genAfter() {
+    return (Math.floor(Math.random() * 2) == 0 ? '' : (' ' + rd(after)));
+}
+
+//Different kinds of quote Generation functions.
+function shortQuote() {
+    var str = rd(rd(subject));
+    if (subj1.indexOf(str) != -1)
+        return (str + ' ' + rd(verbs1) + ' ' + rd(cplt) + genAfter());
+    else if (subj2.indexOf(str) != -1)
+        return (str + ' ' + rd(verbs2) + ' ' + rd(cplt) + genAfter());
+    else
+        return (str + ' ' + rd(verbs3) + ' ' + rd(cplt) + genAfter());
+}
+function longQuote() {
+    return (shortQuote() + rd(conj) + ' ' + shortQuote());
+}
+function swapQuote() {
+  var rand = Math.floor(Math.random() * sing.length);
+  return rd(phrases).replace('!@@', oppSing[rand]).replace('@@', sing[rand]).replace(/##/g, rd(verb));
+}
+
+var gen = [shortQuote, swapQuote];
